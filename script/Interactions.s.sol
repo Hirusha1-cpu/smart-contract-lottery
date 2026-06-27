2e0aef73da4375c781fd02252efe18523abd2cd5
Hirusha1-cpu <hirushafernando121@gmail.com>
2026-06-27 23:10:53 +0530

    Updated files

---
+++ script/Interactions.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// 1. Pragma
+pragma solidity 0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+
+contract CreateSubscription is Script {
+    constructor() {
+        
+    }
+    function createSubscriptionUsingConfig() public returns (uint64) {
+        HelperConfig helperConfig = new HelperConfig();
+        (, , address vrfCoordinator,  , , ) 
+        = helperConfig.activeNetworkConfig();
+        return createSubscription(vrfCoordinator);
+    }
+
+    function createSubscription(address vrfCoordinator) public returns (uint64) {
+        console.log("Creating subscription on ChainId", block.chainid);
+        vm.startBroadcast();
+        uint64 subId =  VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+        console.log("Your SubID is",subId);
+        console.log("Please Update subscription id in HelperConfig");
+        return subId;
+    }
+    function run() external returns (uint64) {
+        return createSubscriptionUsingConfig();
+    }
+}
