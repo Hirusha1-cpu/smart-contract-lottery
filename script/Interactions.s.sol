1a4af9d9079df8613ed7c2f26b4738d2fe0d1292
Hirusha1-cpu <hirushafernando121@gmail.com>
2026-06-28 00:16:45 +0530

    Updated sol file

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
-
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
 contract CreateSubscription is Script {
     constructor() {
@@ -39,6 +39,22 @@ contract FundSubscription is Script{
          HelperConfig helperConfig = new HelperConfig();
         (, , address vrfCoordinator,  , uint64 subId, , address link) 
         = helperConfig.activeNetworkConfig();
+        fundSubscription(vrfCoordinator, subId, link);
+    }
+
+    function fundSubscription(address vrfCoordinator, uint64 subId, address link )  public {
+        console.log("Funding FundSubscription", subId);       
+        console.log("Using vrfCoordinator", vrfCoordinator);
+        console.log("On chainID",block.chainid);    
+        if (block.chainid == 31337) {
+            vm.startBroadcast();
+            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(subId, FUND_AMOUNT);
+            vm.stopBroadcast();
+        } else{
+             vm.startBroadcast();
+             LinkToken(link).transferAndCall(vrfCoordinator, FUND_AMOUNT, abi.encode(subId));
+             vm.stopBroadcast();
+        } 
     }
 
     function run()  external{
